2e93b28fffc0da1359d28197459061b32f6ce365
Vladislav Tsendrovskii <vlad@vltc.net.eu.org>
2017-02-14 16:15:11 +0300

    register uranium ore

--- init.lua
+++ init.lua
@@ -606,3 +606,35 @@ minetest.register_craft({
 	recipe = 'nuclear:uranium_lump',
 })
 
+minetest.register_ore({
+	ore_type = "scatter",
+	ore      = "nuclear:stone_with_uranium",
+	wherein  = "default:stone",
+	clust_scarcity = 9 * 9 * 9,
+	clust_num_ores = 1,
+	clust_size     = 1,
+	y_min          = -255,
+	y_max          = -64,
+})
+
+minetest.register_ore({
+	ore_type = "scatter",
+	ore      = "nuclear:stone_with_uranium",
+	wherein  = "default:stone",
+	clust_scarcity = 9 * 9 * 9,
+	clust_num_ores = 3,
+	clust_size     = 2,
+	y_min          = -1023,
+	y_max          = -256,
+})
+
+minetest.register_ore({
+	ore_type = "scatter",
+	ore      = "nuclear:stone_with_uranium",
+	wherein  = "default:stone",
+	clust_scarcity = 9 * 9 * 9,
+	clust_num_ores = 5,
+	clust_size     = 3,
+	y_min          = -31000,
+	y_max          = -1024,
+})
